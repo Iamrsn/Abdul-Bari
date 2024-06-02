b989c30b7ec094dc3fb203880759fe1a3c1f36fd
roshan <roshankr9894@gmail.com>
2024-06-02 15:41:40 +0530

    Commit

---
+++ Shorted.java
@@ -0,0 +1,18 @@
+public class Shorted {
+    public static boolean isSorted(int[] a) 
+{
+    int i;
+    for(i = 0; i < a.length; i ++);{
+        if (a[i] < a[i+1]) {
+            return true;
+        } else {
+            return false;   
+        }
+    }
+}
+public static void main(String[] args)
+{
+    int ar[] = {3,5,6,7};
+    System.out.println(isSorted(ar));   
+}
+}
